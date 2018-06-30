479a74809266370136a10a584b9e181273f78848
Raul Glogovetan <raul@glogovetan.com>
2018-06-30 12:11:44 +0300

    Add: server handle and store request buffer chunks

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,10 @@
 
 import { createServer } from 'http';
 
-import { MIME_TYPES, STATUS_CODES, RPC_DEFAULT_PORT } from '../defaults';
+import messages from '../messages.json';
+import {
+  MIME_TYPES, STATUS_CODES, REQUEST_TYPES, DEFAULT_HEADERS, RPC_DEFAULT_PORT,
+} from '../defaults';
 
 import type { ServerArgumentsType } from '../flowtypes';
 
@@ -10,9 +13,28 @@ export const start = async (
   { port = RPC_DEFAULT_PORT }: ServerArgumentsType = {},
 ): Promise<Object> => {
   const serverInstance = createServer((request, response) => {
+    let requestDataBuffer: Buffer = Buffer.alloc(0);
+    /*
+     * Request
+     *
+     * @TODO Better handle the error request type
+     */
+    request.on(REQUEST_TYPES.ERROR, (error) => {
+      throw new Error(`${messages.requestFailed}. ${error}`);
+    });
+    request.on(REQUEST_TYPES.DATA, (bufferChunk) => {
+      requestDataBuffer = Buffer.concat([requestDataBuffer, bufferChunk]);
+    });
+    /*
+     * Response
+     */
     response.writeHead(
       STATUS_CODES.OK,
-      { 'Content-Type': MIME_TYPES.JSON },
+      Object.assign(
+        {},
+        DEFAULT_HEADERS,
+        { 'Content-Type': MIME_TYPES.JSON },
+      ),
     );
     response.write('Server is running!');
     response.end();
