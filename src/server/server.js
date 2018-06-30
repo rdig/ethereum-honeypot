e0f4f691740eddc95e8ab6eae0f7c3f2fd9b0563
Raul Glogovetan <raul@glogovetan.com>
2018-06-30 15:03:14 +0300

    Add: handle server response

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,9 @@ import {
   REQUEST_TYPES,
   RPC_DEFAULT_PORT,
   REQUEST_METHODS,
+  STATUS_CODES,
+  DEFAULT_HEADERS,
+  MIME_TYPES,
 } from '../defaults';
 
 import type { ServerArgumentsType } from '../flowtypes';
@@ -15,7 +18,7 @@ import type { ServerArgumentsType } from '../flowtypes';
 export const start = async (
   { port = RPC_DEFAULT_PORT }: ServerArgumentsType = {},
 ): Promise<Object> => {
-  const serverInstance = createServer(({ method, url, ...request }, response) => {
+  const serverInstance = createServer((request, response) => {
     const handleMiscRequest = handleRequest.bind(response);
     let requestDataBuffer: Buffer = Buffer.alloc(0);
     /*
@@ -23,25 +26,51 @@ export const start = async (
      *
      * @TODO Better handle the error request type
      */
-    request.connection.on(REQUEST_TYPES.ERROR, (error) => {
+    request.on(REQUEST_TYPES.ERROR, (error) => {
       throw new Error(`${requestFailed}. ${error}`);
     });
-    request.connection.on(REQUEST_TYPES.DATA, (bufferChunk) => {
+    request.on(REQUEST_TYPES.DATA, (bufferChunk) => {
       requestDataBuffer = Buffer.concat([requestDataBuffer, bufferChunk]);
     });
     /*
      * Response
      */
-    switch (method) {
-      case REQUEST_METHODS.POST:
-        break;
-      case REQUEST_METHODS.OPTIONS:
-        handleMiscRequest(REQUEST_METHODS.OPTIONS);
-        break;
-      default:
+    request.on(REQUEST_TYPES.END, () => {
+      const { method } = request;
+      try {
+        switch (method) {
+          case REQUEST_METHODS.POST: {
+            /*
+             * If the `requestDataBuffer` value is malformed, parsing it to JSON will fail and we
+             * will `catch` it.
+             *
+             * If that's the case, then we return a `Bad Request`, `400` server response.
+             */
+            const payload = JSON.parse(requestDataBuffer.toString());
+            response.writeHead(
+              STATUS_CODES.OK,
+              Object.assign(
+                {},
+                DEFAULT_HEADERS,
+                { 'Content-Type': MIME_TYPES.JSON },
+              ),
+            );
+            response.end(JSON.stringify(payload));
+            break;
+          }
+          case REQUEST_METHODS.OPTIONS: {
+            handleMiscRequest(REQUEST_METHODS.OPTIONS);
+            break;
+          }
+          default: {
+            handleMiscRequest();
+            break;
+          }
+        }
+      } catch (caughtError) {
         handleMiscRequest();
-        break;
-    }
+      }
+    });
   });
   return serverInstance.listen(port);
 };
