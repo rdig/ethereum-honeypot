5a36fcc418185b356ae3a7c3313fa94686504ef8
Raul Glogovetan <raul@glogovetan.com>
2018-06-30 11:13:55 +0300

    Add: access control headers defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,15 +1,21 @@
 /* @flow */
 
-import type { StatusCodesType, MimeTypesType } from './flowtypes';
-
 export const RPC_DEFAULT_PORT: number = 8545;
 
-export const MIME_TYPES: MimeTypesType = {
+export const MIME_TYPES: Object = {
   JSON: 'application/json',
+  PLAIN_TEXT: 'text/plain',
 };
 
-export const STATUS_CODES: StatusCodesType = {
+export const STATUS_CODES: Object = {
   SUCCESS: 200,
+};
+
+export const DEFAULT_HEADERS: Object = {
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Content-Type': MIME_TYPES.PLAIN_TEXT,
 };
 
 const defaults = {
--- src/flowtypes.js
+++ src/flowtypes.js
@@ -1,16 +1,5 @@
 /* @flow */
 
-export type StatusCodesType = {
-  SUCCESS: number,
-};
-
-/*
- * Maybe find a better name for this?
- */
-export type MimeTypesType = {
-  JSON: string,
-};
-
 export type ServerArgumentsType = {
   port?: number,
 };
