03b9518525e03ade603afc5918869d17e59ef66e
Raul Glogovetan <raul@glogovetan.com>
2018-07-07 22:44:20 +0300

    Add: firebase firestore connector

---
+++ src/utils/firebase/defaults.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+export const DEFAULT_COLLECTION: string = 'rpc-requests';
+
+const firebaseDefaults: Object = {
+  DEFAULT_COLLECTION,
+};
+
+export default firebaseDefaults;
---
+++ src/utils/firebase/firebaseFirestoreAddData.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import firestoreDatabase from './firebaseFirestoreConnector';
+
+import { DEFAULT_COLLECTION } from './defaults';
+
+/**
+ * Write a new object (under a randomly generated id) into the specified collection.
+ * (This is using the "still beta" Cloud Firestore Database API)
+ *
+ * @param {object} honeypotObject the object to write to the firestore database
+ * @param {string} collection optional collection name, defaults to `rpc-requests`
+ *
+ * The above arguments are passed in as props of and Object
+ */
+export const firebaseFirestoreAddData = async ({
+  honeypotObject,
+  collection = DEFAULT_COLLECTION,
+}: Object): Promise<*> => {
+  try {
+    return firestoreDatabase.collection(collection).doc().set(honeypotObject);
+  } catch (caughtError) {
+    throw new Error(`[${new Date().toString()}] Could not add data to the Cloud Firestore Database. Check the object you were trying to add: ${JSON.stringify(honeypotObject)}`);
+  }
+};
+
+export default firebaseFirestoreAddData;
