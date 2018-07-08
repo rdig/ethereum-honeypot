c01ecad7f4b2aa2c2d5b92415ce1edf5db4996f0
Raul Glogovetan <raul@glogovetan.com>
2018-07-08 16:32:26 +0300

    Add: `getIpFromRequest` util method

---
+++ src/utils/getIpFromRequest/defaults.js
@@ -0,0 +1,7 @@
+/* @flow */
+
+export const IPV6_LOCAL: string = '::ffff:';
+
+export const PLACEHOLDER_IP: string = '0.0.0.0';
+
+export const X_FORWARDED_HEADER: string = 'x-forwarded-for';
---
+++ src/utils/getIpFromRequest/getIpFromRequest.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import { IPV6_LOCAL, PLACEHOLDER_IP, X_FORWARDED_HEADER } from './defaults';
+
+/**
+ * Get the request IP address from the request object
+ * (It's found in various places across the request object depending on how the request
+ * was made: direct, proxyies, vpns, etc...)
+ *
+ * @method getIpFromRequest
+ *
+ * @param {Object} requestObject the node `http` request object
+ *
+ * @return {string} the extract ip address (either ipv4 or ipv6)
+ */
+export const getIpFromRequest = (requestObject: Object): string => {
+  if (
+    !(requestObject && (requestObject.headers || requestObject.connection || requestObject.socket))
+  ) {
+    /*
+     * @TODO Create a better error logging util
+     */
+    throw new Error(`[${new Date().toString()}] Could not get IP address information from the Request Object: ${JSON.stringify(requestObject)}`);
+  }
+  const rawIpString: string = (
+    requestObject.headers[X_FORWARDED_HEADER]
+    || requestObject.connection.remoteAddress
+    || requestObject.socket.remoteAddress
+    || (requestObject.connection.socket
+      ? requestObject.connection.socket.remoteAddress : PLACEHOLDER_IP)
+  );
+  if (rawIpString.indexOf(IPV6_LOCAL) >= 0) {
+    return rawIpString.replace(IPV6_LOCAL, '');
+  }
+  return rawIpString;
+};
+
+export default getIpFromRequest;
