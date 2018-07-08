318022a305c60180af0a3ca04a3fbace8edab21b
Raul Glogovetan <raul@glogovetan.com>
2018-07-08 13:55:18 +0300

    Add: firestore query data

---
+++ src/utils/firebase/firebaseFirestoreGetData.js
@@ -0,0 +1,49 @@
+/* @flow */
+
+import firestoreDatabase from './firebaseFirestoreConnector';
+
+import { DEFAULT_COLLECTION } from './defaults';
+
+/**
+ * Get all obects from a specified firestore collection.
+ * (This is useful to check for previously entered values and set the same geolocation data to the
+ * new request, avoiding the need to hit the geo api again)
+ *
+ * @method firebaseFirestoreGetData
+ *
+ * @param {string} fieldPath name of the field to search for
+ * @param {string} opStr comparison operator (as a string)
+ * @param {any} value any value to test for
+ * @param {number} limit optinal limit the number of returned values
+ * @param {string} collection optional collection name, defaults to `rpc-requests`
+ *
+ * The above arguments are passed in as props of and Object
+ *
+ * @return {Object} the query snapshot
+ * See: https://cloud.google.com/nodejs/docs/reference/firestore/0.14.x/Query
+ */
+export const firebaseFirestoreGetData = async ({
+  fieldPath,
+  opStr,
+  value,
+  limit,
+  /*
+   * @TODO Different collections if we're in a development environment
+   */
+  collection = DEFAULT_COLLECTION,
+}: Object): Promise<*> => {
+  try {
+    const firestoreQuery = firestoreDatabase.collection(collection).where(fieldPath, opStr, value);
+    if (limit) {
+      return firestoreQuery.limit(limit).get();
+    }
+    return firestoreQuery.get();
+  } catch (caughtError) {
+    /*
+     * @TODO Create a better error logging util
+     */
+    throw new Error(`[${new Date().toString()}] Could not get data from the Cloud Firestore Database. Check the query you were trying to use: ${fieldPath} ${opStr} ${value}. Error: ${caughtError.message}`);
+  }
+};
+
+export default firebaseFirestoreGetData;
--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -1,3 +1,5 @@
 /* @flow */
 
 export { firebaseFirestoreAddData, default } from './firebaseFirestoreAddData';
+
+export { firebaseFirestoreGetData } from './firebaseFirestoreGetData';
