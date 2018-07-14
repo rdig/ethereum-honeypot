a4fc1d541a66ddf952adb403dd7076a75cec2a1e
Raul Glogovetan <raul@glogovetan.com>
2018-07-14 20:03:34 +0300

    Fix: `errorLogger` util add method comment block

--- src/utils/errorLogger/errorLogger.js
+++ src/utils/errorLogger/errorLogger.js
@@ -1,9 +1,19 @@
 /* @flow */
 
+/**
+ * Throw and error with a log-formatted message
+ * (Which will be logged inside a file on the system)
+ *
+ * @method errorLogger
+ *
+ * @param {string} userMessage A message to inform the user about the problem
+ * @param {any} errorValue Optional variable value (if it's an Object, it will be serialized)
+ * @param {string} errorMessage Optional message that came from a try-catch block
+ */
 export const errorLogger = (
   userMessage: string | void,
-  errorValue: any,
-  errorMessage: string | void,
+  errorValue?: any,
+  errorMessage?: string | void,
 ) => {
   let loggerMessage: string;
   if (!userMessage) {
