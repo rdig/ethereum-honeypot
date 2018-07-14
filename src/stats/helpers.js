1873b4eef700d74646f1a2c1076cc871ea5a665a
Raul Glogovetan <raul@glogovetan.com>
2018-07-15 00:50:06 +0300

    Add: `getInitialDataObject` stats helper method

--- src/stats/helpers.js
+++ src/stats/helpers.js
@@ -1,21 +1,64 @@
 /* @flow */
 
 import { errorLogger } from '../utils/errorLogger';
-import { firebaseFirestoreAddData, firebaseFirestoreGetData } from '../utils/firebase';
+import {
+  firebaseFirestoreAddData,
+  firebaseFirestoreGetData,
+} from '../utils/firebase';
 
-import { UNDEFINED } from './defaults';
 import { STATS_COLLECTION } from '../utils/firebase/defaults';
+import { UNDEFINED } from './defaults';
+
+const propNameCountInitial: number = 1;
+
+/**
+ * Helper method to generate an stats data object for a new prop name (one that hasn't been added before)
+ *
+ * @method getInitialDataObject
+ *
+ * @param {string} propName Stat name inside the document id to search for (basically the individual value inside the stats collection)
+ * @param {Objecct} additionalData An optional object to add to the individual stat value, besides the count
+ *
+ * The above params are passed in as props of an Object.
+ */
+export const getInitialDataObject = ({
+  propName,
+  additionalData,
+}: {
+  propName: string,
+  additionalData?: Object,
+}): Object => {
+  /*
+   * If we don't have additional data, just count the entries,
+   * and since this is the first one, it's 1
+   */
+  const dataObject: Object = {};
+  dataObject[propName] = propNameCountInitial;
+  /*
+   * But if we have additional data to add, the counter gets it's own prop name.
+   */
+  if (additionalData && additionalData instanceof Object) {
+    dataObject[propName] = Object.assign(
+      {},
+      { count: propNameCountInitial },
+      additionalData,
+    );
+  }
+  return dataObject;
+};
 
 /**
  * Helper method to reduce code repetition when counting stats.
  * This will check if the document Id exists, if it is, it increments it, otherwise it adds it in.
-
+ *
  * @method statsGenerator
-
+ *
  * @param {string} collection Collection name, defaults to `rpc-requests-stats`
  * @param {string} documentId Document id to search for (basically the current stats collection)
  * @param {string} propName Stat name inside the document id to search for (basically the individual value inside the stats collection)
  * @param {Objecct} additionalData An optional object to add to the individual stat value, besides the count
+ *
+ * The above params are passed in as props of an Object.
  */
 export const statsGenerator = async ({
   collection = STATS_COLLECTION,
@@ -34,7 +77,6 @@ export const statsGenerator = async ({
   if (!propName) {
     errorLogger('Stats prop name was not set', propName || UNDEFINED);
   }
-  const propNameCountInitial: number = 1;
   const availableStatsFireabaseQuery: Object = await firebaseFirestoreGetData({
     collection,
     documentId,
@@ -43,24 +85,8 @@ export const statsGenerator = async ({
    * Document id does not exist, this is the first entry of it's kind, so we add it.
    */
   if (!availableStatsFireabaseQuery.exists) {
-    /*
-     * If we don't have additional data, just count the entries,
-     * and since this is the first one, it's 1
-     */
-    const dataObject: Object = {};
-    dataObject[propName] = propNameCountInitial;
-    /*
-     * But if we have additional data to add, the counter gets it's own prop name.
-     */
-    if (additionalData && additionalData instanceof Object) {
-      dataObject[propName] = Object.assign(
-        {},
-        { count: propNameCountInitial },
-        additionalData,
-      );
-    }
     return firebaseFirestoreAddData({
-      dataObject,
+      dataObject: getInitialDataObject({ propName, additionalData }),
       collection,
       documentId,
     });
@@ -109,10 +135,6 @@ export const statsGenerator = async ({
   /*
    * Document id exists, but the current prop name does not exist. We add it now
    */
-
-  /*
-   * Document id exists, and the current prop name exists, so we increment it
-   */
   let propNameData: Object | number;
   /*
   * If we have additional data to add, the counter gets it's own prop name.
@@ -139,5 +161,3 @@ export const statsGenerator = async ({
     documentId,
   });
 };
-
-export default statsGenerator;
